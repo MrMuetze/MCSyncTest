8b2554132b7f9f5b352188829e2ba3bbc2dddb8d
Björn Wieczoreck <bjoern.wieczoreck@giga-infosystems.com>
2024-01-31 14:56:35 +0100

    Initial Commit

---
+++ MCSyncTest/MCSyncTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCSyncTestApp.swift
+//  MCSyncTest
+//
+//  Created by Björn Wieczoreck on 31.01.24.
+//
+
+import SwiftUI
+
+@main
+struct MCSyncTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
